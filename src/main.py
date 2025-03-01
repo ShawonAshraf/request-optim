64c692ef213bd383c4dc73eadac36aed98f0b44d
Shawon Ashraf <7804029+ShawonAshraf@users.noreply.github.com>
2025-03-02 00:03:51 +0100

    main script

--- src/main.py
+++ src/main.py
@@ -4,23 +4,26 @@ from loguru import logger
 
 
 # Example usage:
-async def main():
+async def run_optimizer():
     optimizer = HttpGetOptimizer()
-    
+
     url_root = "https://httpbin.org/get"
     # create 50 requests
     test_urls = [url_root + f"?{i}" for i in range(50)]
-    
+
     # TODO: add argparse to take the number of requests as cmd args
     # TODO: add different endpoints
     # TODO: yes also add unit tests
-    
+
     results = await asyncio.gather(*(optimizer.get(tu) for tu in test_urls))
     logger.info(f"Processed {len(results)} requests.")
-    
 
-    
     await optimizer.close()
 
+
+def main():
+    asyncio.run(run_optimizer())
+
+
 if __name__ == "__main__":
-    asyncio.run(main())
+    main()
