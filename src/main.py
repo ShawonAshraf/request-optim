b3c36fee217a6eba227b54731a5f9313846eede7
Shawon Ashraf <7804029+ShawonAshraf@users.noreply.github.com>
2025-03-02 00:44:41 +0100

    image request

--- src/main.py
+++ src/main.py
@@ -12,7 +12,8 @@ async def run_optimizer():
 
     urls = ["https://httpbin.org/get"] * N_REQS + \
         ["https://httpbin.org/status/404"] * N_REQS + \
-        ["https://httpbin.org/ip"] * N_REQS
+        ["https://httpbin.org/ip"] * N_REQS + \
+        ["https://httpbin.org/image"] * N_REQS
 
     async with optimizer:
         results = await asyncio.gather(*(optimizer.get(url) for url in urls))
